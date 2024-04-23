02eac2ba77dfd2b65b79bfb711f29b3bf031fcfb
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-04-23 09:49:07 +0100

    improving performance by lazy loading header image

--- src/containers/header/index.tsx
+++ src/containers/header/index.tsx
@@ -1,15 +1,18 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import { Navbar } from "../../components/navigation";
 import { Hero } from "../../components/hero";
 import headerBg from "../../assets/headerBg.svg";
 import mobileHeaderBg from "../../assets/headerMobile.svg";
 import { CalendlyRef } from "../../types/app.type";
+import { LazyBg } from "../../configs/lazyBg";
 
 export const Header: React.FC<CalendlyRef> = ({ calendlyRef }) => {
+    const matches = useMediaQuery('(max-width:768px)');
+    const bgImage = matches ? LazyBg(mobileHeaderBg) : LazyBg(headerBg);
     return (
         <Box
             sx={{
-                backgroundImage: { mobile: `url(${mobileHeaderBg})`, tablet: `url(${headerBg})` },
+                backgroundImage: `url(${bgImage})`,
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
                 backgroundPosition: { mobile: "center", tablet: "bottom right" },
