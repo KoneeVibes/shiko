0696831a5dab7e4e8656412d130a2d828655e1c4
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-04-23 10:10:11 +0100

    improving performance by lazy loading header image - II

--- src/configs/lazyBg.tsx
+++ src/configs/lazyBg.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const LazyBg = (src: any) => {
-    const [sourceLoaded, setSourceLoaded] = useState(null)
+    const [sourceLoaded, setSourceLoaded] = useState("")
 
     useEffect(() => {
         const img = new Image()
